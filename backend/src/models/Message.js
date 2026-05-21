9d0f88db4d500e28e9a6ab8da56572d3b408b321
Do Hoai Anh <anhd78428@gmail.com>
2026-05-21 17:27:58 +0700

    api sendDirect message

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,26 +1,33 @@
 import mongoose from "mongoose";
-const messageSchema = new mongoose.Schema({
-    conversationId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Conversation",
-        required: true,
-        index: true
+
+const messageSchema = new mongoose.Schema(
+    {
+        conversationId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Conversation",
+            required: true,
+            index: true,
+        },
+        senderId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        content: {
+            type: String,
+            trim: true,
+        },
+        imgUrl: {
+            type: String,
+        },
     },
-    senderId: {
-        type: mongoose.Schema.types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    content: {
-        type: String,
-        trim: true,
-    },
-    imgUrl: {
-        type: string
-    },
-}, {
-    timestamps: true
-})
-messageShema.index({ conversationId: 1, createdAt: -1 });
+    {
+        timestamps: true,
+    }
+);
+
+messageSchema.index({ conversationId: 1, createdAt: -1 });
+
 const Message = mongoose.model("Message", messageSchema);
+
 export default Message;
