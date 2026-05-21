b3e7e42fd02e5b530a86c4af733f2115c2034aff
Do Hoai Anh <anhd78428@gmail.com>
2026-05-21 19:46:15 +0700

    middlaware checkFriendShip

---
+++ backend/src/controllers/conversationController.js
@@ -0,0 +1,9 @@
+export const createConversation = async (req, res) => {
+
+}
+export const getConversations = async (req, res) => {
+
+}
+export const getMessages = async (req, res) => {
+
+}
--- backend/src/routes/messageRoute.js
+++ backend/src/routes/messageRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { sendDirectMessage, sendGroupMessage } from "../controllers/mesageController.js";
+import { checkFriendship } from "../middlewares/friendMiddleware.js";
 
 const router = express.Router();
 
-router.post("/direct", sendDirectMessage);
+router.post("/direct", checkFriendship, sendDirectMessage);
 router.post("/group", sendGroupMessage);
 
 export default router;
