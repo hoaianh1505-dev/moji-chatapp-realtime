16b4cbd6f0ceb416655e1dd2fdc2b52843df6d3b
Do Hoai Anh <anhd78428@gmail.com>
2026-05-27 13:05:10 +0700

    create layout Sidebar

---
+++ frontend/src/components/chat/ChatWindowLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatWindowLayout = () => {
+    return (
+        <div>
+            chat window layout
+        </div>
+    )
+}
+
+export default ChatWindowLayout
